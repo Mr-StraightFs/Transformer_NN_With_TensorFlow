c16559f8833c723cc44fc43410cb0b2236203648
Mr-StraightFs <amin.lh.mba@gmail.com>
2021-09-17 17:22:12 +0100

    Get the angles for the positional encoding Function

--- Tranformers_Network.py
+++ Tranformers_Network.py
@@ -9,3 +9,23 @@ from transformers import DistilBertTokenizerFast #, TFDistilBertModel
 from transformers import TFDistilBertForTokenClassification
 from tqdm import tqdm_notebook as tqdm
 
+
+def get_angles(pos, k, d):
+    """
+    Get the angles for the positional encoding
+
+    Arguments:
+        pos -- Column vector containing the positions [[0], [1], ...,[N-1]]
+        k --   Row vector containing the dimension span [[0, 1, 2, ..., d-1]]
+        d(integer) -- Encoding size
+
+    Returns:
+        angles -- (pos, d) numpy array
+    """
+
+    # Get i from dimension span k
+    i = None
+    # Calculate the angles using pos, i and d
+    angles = None
+
+    return angles
